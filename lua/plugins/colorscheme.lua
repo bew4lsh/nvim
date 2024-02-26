8ec167d64c5da905a1f6d18e2bbd7b1ec9ad5ba1
bewalsh <bew4lsh@gmail.com>
2024-02-25 22:54:35 -0500

    added colorscheme options, updated mkdnflow config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,32 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-  { "rose-pine/neovim", name = "rose-pine" },
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "rose-pine",
     },
   },
+  { "ellisonleao/gruvbox.nvim" },
+  { "rose-pine/neovim", name = "rose-pine" },
+  -- Configure LazyVim to load gruvbox
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  { "rktjmp/lush.nvim" },
+  -- { "nyoom-engineering/oxocarbon.nvim", opts = { background = "dark" } },
+  { "olivercederborg/poimandres.nvim" },
+  { "JoosepAlviste/palenightfall.nvim" },
+  { "xero/miasma.nvim" },
+  { "yonlu/omni.vim" },
+  { "lmburns/kimbox" },
+  { "shrikecode/kyotonight.vim" },
+  { "numToStr/Sakura.nvim" },
+  { "luisiacc/the-matrix.nvim" },
+  { "Scysta/pink-panic.nvim" },
 }
--- lua/plugins/mkdnflow.lua
+++ lua/plugins/mkdnflow.lua
@@ -2,7 +2,15 @@ return {
   {
     "jakewvincent/mkdnflow.nvim",
     config = function()
-      require("mkdnflow").setup()
+      require("mkdnflow").setup({
+        perspective = {
+          priority = "root",
+          fallback = "current",
+          root_tell = "index.md",
+          nvim_wd_heel = true,
+          update = true,
+        },
+      })
     end,
   },
 }
