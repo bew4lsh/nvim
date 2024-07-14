e3678fd6cd33951585921ef92c58fd9a5044977d
bew4lsh <bew4lsh@gmail.com>
2024-07-14 01:14:31 -0400

    Added a new keybinding to send file content in the ChatGPT plugin.

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -6,7 +6,35 @@ return {
     },
     event = "VeryLazy",
     config = function()
-      require("chatgpt").setup()
+      require("chatgpt").setup({
+        chat = {
+          keymaps = {
+            close = "<C-c>",
+            yank_last = "<C-y>",
+            yank_last_code = "<C-k>",
+            scroll_up = "<C-u>",
+            scroll_down = "<C-d>",
+            new_session = "<C-n>",
+            cycle_windows = "<Tab>",
+            cycle_modes = "<C-f>",
+            next_message = "<C-j>",
+            prev_message = "<C-k>",
+            select_session = "<Space>",
+            rename_session = "r",
+            delete_session = "d",
+            draft_message = "<C-r>",
+            edit_message = "e",
+            delete_message = "d",
+            toggle_settings = "<C-o>",
+            toggle_sessions = "<C-p>",
+            toggle_help = "<C-h>",
+            toggle_message_role = "<C-r>",
+            toggle_system_role_open = "<C-s>",
+            stop_generating = "<C-x>",
+            send_file_content = "<C-f>",  -- New keybinding
+          },
+        },
+      })
     end,
     dependencies = {
       "MunifTanjim/nui.nvim",
