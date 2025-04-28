eb5e7d2a5a07b340f170a31f38a07ca2643b091f
William Walsh <william.j.walsh@accenture.com>
2025-04-27 21:37:32 -0400

    Updates for markdown rendering, yazi keymap

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -46,6 +46,43 @@ return {
           "RenderMarkdownH6",
         },
       },
+      checkbox = {
+        -- Turn on / off checkbox state rendering.
+        enabled = true,
+        -- Additional modes to render checkboxes.
+        render_modes = false,
+        -- Padding to add to the right of checkboxes.
+        right_pad = 1,
+        unchecked = {
+          -- Replaces '[ ]' of 'task_list_marker_unchecked'.
+          icon = "󰄱 ",
+          -- Highlight for the unchecked icon.
+          highlight = "RenderMarkdownUnchecked",
+          -- Highlight for item associated with unchecked checkbox.
+          scope_highlight = nil,
+        },
+        checked = {
+          -- Replaces '[x]' of 'task_list_marker_checked'.
+          icon = "✔️ ",
+          -- Highlight for the checked icon.
+          highlight = "RenderMarkdownChecked",
+          -- Highlight for item associated with checked checkbox.
+          scope_highlight = "@markup.italic",
+        },
+        -- Define custom checkbox states, more involved, not part of the markdown grammar.
+        -- As a result this requires neovim >= 0.10.0 since it relies on 'inline' extmarks.
+        -- The key is for healthcheck and to allow users to change its values, value type below.
+        -- | raw             | matched against the raw text of a 'shortcut_link'           |
+        -- | rendered        | replaces the 'raw' value when rendering                     |
+        -- | highlight       | highlight for the 'rendered' icon                           |
+        -- | scope_highlight | optional highlight for item associated with custom checkbox |
+        -- stylua: ignore
+        custom = {
+            -- cancelled = { raw = '[-]', rendered = '🚫 ', highlight = 'RenderMarkdownTodo', scope_highlight = nil },
+            cancelled = { raw = '[-]', rendered = '⊘ ', highlight = 'RenderMarkdownTodo', scope_highlight = "@markup.strikethrough" },
+            todo = { raw = '[/]', rendered = '↗ ', highlight = 'RenderMarkdownTodo', scope_highlight = "@markup.strong" },
+        },
+      },
     },
     ft = { "markdown", "norg", "rmd", "org" },
     config = function(_, opts)
