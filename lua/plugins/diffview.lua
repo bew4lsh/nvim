a6d7b5867882df4f11886a5914ceb0dd60dab753
Ubuntu <lia@dev-vm.mcd0gzuzxeyebklsxz1zdftztg.bx.internal.cloudapp.net>
2025-07-21 11:18:12 +0000

    feat: adding keybinds for diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,5 +1,18 @@
 return {
   {
     "sindrets/diffview.nvim",
+    keys = {
+      { "<leader>gvo", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
+      { "<leader>gvc", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+      { "<leader>gvh", "<cmd>DiffviewFileHistory<cr>", desc = "File History" },
+      { "<leader>gvf", "<cmd>DiffviewToggleFiles<cr>", desc = "Toggle File Panel" },
+      { "<leader>gvF", "<cmd>DiffviewFileHistory %<cr>", desc = "Current File History" },
+    },
+    config = function()
+      local wk = require("which-key")
+      wk.add({
+        { "<leader>gv", group = "diffview" },
+      })
+    end,
   },
 }
